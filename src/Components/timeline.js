78ea286c644129f3e6b0921f5562e8208b257c4f
Angie Leiva <38085631+ancarolm@users.noreply.github.com>
2021-03-15 12:28:14 -0600

    Changes

---
+++ src/Components/timeline.js
@@ -0,0 +1,17 @@
+import timelineData from "../data";
+import timelineitem from "./timelineintem";
+
+const timeline = () => timelineData.length > 0 && (
+
+    <div className= "timeline-container">
+        {timelineData.map((data, idx) => (
+
+                <timelineitem data={data} key= {idx}/>
+
+
+        ))}
+
+    </div>
+);
+
+export default timeline;
---
+++ src/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+
+        text: "this is a proof",
+        date: "march 21 2021",
+        category: {
+            tag: "medium",
+            color: "#00000"
+        },
+        link: {
+            url: "https://github.com/ancarolm/Timeline",
+            text: "Read More"
+        }
+
+    },
+    {
+        text: "this is a proof",
+        date: "march 21 2021",
+        category: {
+            tag: "medium",
+            color: "#00000"
+        },
+        link: {
+            url: "https://github.com/ancarolm/Timeline",
+            text: "Read More"
+        }
+
+    }
+]
+
+export default data;
+
